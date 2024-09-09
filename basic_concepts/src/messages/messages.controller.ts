574918423183a69affc4b8696152b60e93fc3de0
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-09-09 08:57:19 -0300

    Nest Basic Concepts: Completing CRUD example and using query params on routes

--- basic_concepts/src/messages/messages.controller.ts
+++ basic_concepts/src/messages/messages.controller.ts
@@ -1,10 +1,23 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
+  @HttpCode(HttpStatus.OK)
   @Get()
-  findAll() {
-    return 'Returns all messages';
+  findAll(@Query() pagination: { limit: string; offset: string }) {
+    const { limit = 10, offset = 0 } = pagination;
+    return `Returns all messages. Limit: ${limit}, Offset: ${offset}`;
   }
 
   @Get(':id')
@@ -18,5 +31,21 @@ export class MessagesController {
     return `This route creates a message for ${body.user}:
     ${body.message}
     `;
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() body: { message?: string; user?: string },
+  ) {
+    return {
+      id,
+      ...body,
+    };
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Deleted message with ID: ${id}`;
   }
 }
