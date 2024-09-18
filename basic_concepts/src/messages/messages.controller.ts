8b4b37571bf5586fd647beee9b9f5728361b9bb2
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-09-18 16:58:18 -0300

    Nest Basic Concepts: Understanding Dependency Injection in Nest

--- basic_concepts/src/messages/messages.controller.ts
+++ basic_concepts/src/messages/messages.controller.ts
@@ -5,6 +5,7 @@ import {
   Get,
   HttpCode,
   HttpStatus,
+  Inject,
   Param,
   Patch,
   Post,
@@ -14,14 +15,31 @@ import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { RegexProtocol } from 'src/common/regex/regex.protocol';
+import {
+  ONLY_LOWERCASE_LETTERS_REGEX,
+  REMOVE_SPACES_REGEX,
+  SERVER_NAME,
+} from './messages.constants';
 
 @Controller('messages')
 export class MessagesController {
-  constructor(private readonly messagesService: MessagesService) {}
+  constructor(
+    private readonly messagesService: MessagesService,
+    @Inject(SERVER_NAME)
+    private readonly serverName: string,
+    @Inject(REMOVE_SPACES_REGEX)
+    private readonly removeSpacesRegex: RegexProtocol,
+    @Inject(ONLY_LOWERCASE_LETTERS_REGEX)
+    private readonly onlyLowercaseLettersRegex: RegexProtocol,
+  ) {}
 
   @HttpCode(HttpStatus.OK)
   @Get()
   async findAll(@Query() pagination: PaginationDto) {
+    console.log(this.removeSpacesRegex.execute(this.serverName));
+    console.log(this.onlyLowercaseLettersRegex.execute(this.serverName));
+
     const messages = await this.messagesService.findAll(pagination);
 
     return messages;
--- basic_concepts/src/messages/messages.module.ts
+++ basic_concepts/src/messages/messages.module.ts
@@ -4,10 +4,31 @@ import { MessagesService } from './messages.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from './entities/message.entity';
 import { UsersModule } from 'src/users/users.module';
+import { RemoveSpacesRegex } from 'src/common/regex/remove-spaces.regex';
+import { OnlyLowercaseLettersRegex } from 'src/common/regex/only-lowercase-letters.regex';
+import {
+  ONLY_LOWERCASE_LETTERS_REGEX,
+  REMOVE_SPACES_REGEX,
+  SERVER_NAME,
+} from './messages.constants';
 
 @Module({
   controllers: [MessagesController],
-  providers: [MessagesService],
+  providers: [
+    MessagesService,
+    {
+      provide: SERVER_NAME,
+      useValue: 'My Name Is NestJS',
+    },
+    {
+      provide: ONLY_LOWERCASE_LETTERS_REGEX,
+      useClass: OnlyLowercaseLettersRegex,
+    },
+    {
+      provide: REMOVE_SPACES_REGEX,
+      useClass: RemoveSpacesRegex,
+    },
+  ],
   imports: [TypeOrmModule.forFeature([Message]), UsersModule],
 })
 export class MessagesModule {}
