13d441156d80d3fa7bb0eae5b0c9062947612e9a
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-09-20 14:16:02 -0300

    Nest Basic Concepts: Using Dot Env to save variables

--- basic_concepts/src/app/app.module.ts
+++ basic_concepts/src/app/app.module.ts
@@ -4,18 +4,20 @@ import { AppService } from './app.service';
 import { MessagesModule } from 'src/messages/messages.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from 'src/users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'postgres',
-      database: 'postgres',
-      password: 'postgres',
-      autoLoadEntities: true,
-      synchronize: true,
+      type: process.env.DATABASE_TYPE as 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      database: process.env.DATABASE_DATABASE,
+      password: process.env.DATABASE_PASSWORD,
+      autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES),
+      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE),
     }),
     MessagesModule,
     UsersModule,
--- basic_concepts/src/messages/messages.module.ts
+++ basic_concepts/src/messages/messages.module.ts
@@ -4,18 +4,10 @@ import { MessagesService } from './messages.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from './entities/message.entity';
 import { UsersModule } from 'src/users/users.module';
-import { MyDynamicModule } from 'src/my-dynamic/my-dynamic.module';
 
 @Module({
   controllers: [MessagesController],
   providers: [MessagesService],
-  imports: [
-    TypeOrmModule.forFeature([Message]),
-    UsersModule,
-    MyDynamicModule.register({
-      apiKey: 'API_KEY',
-      apiUrl: 'http://apiUrl.com',
-    }),
-  ],
+  imports: [TypeOrmModule.forFeature([Message]), UsersModule],
 })
 export class MessagesModule {}
