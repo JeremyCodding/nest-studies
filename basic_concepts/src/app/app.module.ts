93a2fd661964539a893ad2c3af36edaa7aa73626
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-09-22 16:54:15 -0300

    Nest Basic Concepts: Using ConfigModule to load variables from config file

--- basic_concepts/src/app/app.module.ts
+++ basic_concepts/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { MessagesModule } from 'src/messages/messages.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from 'src/users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from '@hapi/joi';
+import appConfig from './app.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      load: [appConfig],
       validationSchema: Joi.object({
         DATABASE_TYPE: Joi.required(),
         DATABASE_HOST: Joi.required(),
@@ -21,15 +23,33 @@ import * as Joi from '@hapi/joi';
         DATABASE_SYNCHRONIZE: Joi.number().min(0).max(1).default(0),
       }),
     }),
-    TypeOrmModule.forRoot({
-      type: process.env.DATABASE_TYPE as 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USERNAME,
-      database: process.env.DATABASE_DATABASE,
-      password: process.env.DATABASE_PASSWORD,
-      autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES),
-      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE),
+    // TypeOrmModule.forRoot({
+    //   type: process.env.DATABASE_TYPE as 'postgres',
+    //   host: process.env.DATABASE_HOST,
+    //   port: +process.env.DATABASE_PORT,
+    //   username: process.env.DATABASE_USERNAME,
+    //   database: process.env.DATABASE_DATABASE,
+    //   password: process.env.DATABASE_PASSWORD,
+    //   autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES),
+    //   synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE),
+    // }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: configService.get<'postgres'>('database.type'),
+          host: configService.get<string>('database.host'),
+          port: +configService.get<number>('database.port]'),
+          username: configService.get<string>('database.username'),
+          database: configService.get<string>('database.database'),
+          password: configService.get<string>('database.password'),
+          autoLoadEntities: configService.get<boolean>(
+            'database.autoLoadEntities',
+          ),
+          synchronize: configService.get<boolean>('database.synchronize'),
+        };
+      },
     }),
     MessagesModule,
     UsersModule,
