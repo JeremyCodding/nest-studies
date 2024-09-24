b87ab457f5d4e40b96f568d611f8a54496e6b693
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-09-24 11:19:54 -0300

    Nest Basic Concepts: Creating user validation in login method

--- basic_concepts/src/auth/auth.service.ts
+++ basic_concepts/src/auth/auth.service.ts
@@ -1,9 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
+import { Repository } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { HashingService } from './hashing/hashing.service';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly hashingService: HashingService,
+  ) {}
+
   async login(loginDto: LoginDto) {
-    return loginDto;
+    const user = await this.userRepository.findOneBy({
+      email: loginDto.email,
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Email or Password are invalid.');
+    }
+
+    const passwordIsValid = await this.hashingService.compare(
+      loginDto.password,
+      user.passwordHash,
+    );
+
+    if (!passwordIsValid) {
+      throw new UnauthorizedException('Email or Password are invalid.');
+    }
+
+    return {
+      message: 'User validated',
+    };
   }
 }
